af67bf01ee66c9e6cd7a56fb64d68f76e89e7dd4
SamHaggans <sam@samhaggans.com>
2021-07-15 15:18:15 -0600

    Run main code only when called as main

--- main.py
+++ main.py
@@ -3,35 +3,36 @@ import data
 import pathing
 import display
 
-data.initialize()
+if __name__ == '__main__':
+    data.initialize()
 
-while True:
-    lat1 = int(input("Enter starting lat: "))
-    lon1 = int(input("Enter starting lon: "))
-    lat2 = int(input("Enter ending lat: "))
-    lon2 = int(input("Enter ending lon: "))
+    while True:
+        lat1 = int(input("Enter starting lat: "))
+        lon1 = int(input("Enter starting lon: "))
+        lat2 = int(input("Enter ending lat: "))
+        lon2 = int(input("Enter ending lon: "))
 
-    coords = data.get_coords()
+        coords = data.get_coords()
 
-    start = utils.nearest_coord_to_lat_lon(lat1, lon1)[0]
+        start = utils.nearest_coord_to_lat_lon(lat1, lon1)[0]
 
-    end = utils.nearest_coord_to_lat_lon(lat2, lon2)[0]
+        end = utils.nearest_coord_to_lat_lon(lat2, lon2)[0]
 
-    start_coords = [coords['lat'][start[0]][start[1]], coords['lon'][start[0]][start[1]]]
-    end_coords = [coords['lat'][end[0]][end[1]], coords['lon'][end[0]][end[1]]]
+        start_coords = [coords['lat'][start[0]][start[1]], coords['lon'][start[0]][start[1]]]
+        end_coords = [coords['lat'][end[0]][end[1]], coords['lon'][end[0]][end[1]]]
 
-    print(f'Finding path between {start_coords} and {end_coords}')
+        print(f'Finding path between {start_coords} and {end_coords}')
 
-    path_info = pathing.find_path(start, end)
-    path = path_info['path']['path']
-    path_coords = path_info['path']['path_coords']
-    path_length = path_info['path']['distance']
-    path_cost = path_info['difficulty']
-    net_distance = path_info['distance']
+        path_info = pathing.find_path(start, end)
+        path = path_info['path']['path']
+        path_coords = path_info['path']['path_coords']
+        path_length = path_info['path']['distance']
+        path_cost = path_info['difficulty']
+        net_distance = path_info['distance']
 
-    print("Path length: " + str(path_length))
-    print("Path cost: " + str(path_cost))
-    print("Straight line distance traveled: " + str(net_distance))
-    print(path_coords)
+        print("Path length: " + str(path_length))
+        print("Path cost: " + str(path_cost))
+        print("Straight line distance traveled: " + str(net_distance))
+        print(path_coords)
 
-    display.show_path_plot(path, 0, 500)
+        display.show_path_plot(path, 0, 500)
